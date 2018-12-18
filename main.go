22ae0a0f9180675310b83688d9f439cbef68482c
vitr <vit@vvitr.com>
2018-12-18 22:02:16 +1000

    Use user and password from environment

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"github.com/goji/httpauth"
 	"github.com/zenazn/goji"
 	"net/http"
+	"os"
 )
 
 func main() {
-	goji.Use(httpauth.SimpleBasicAuth("user", "password"))
+	u := os.Getenv("USER")
+	p := os.Getenv("PASSWORD")
+	if len(u) > 0 && len(p) > 0 {
+		goji.Use(httpauth.SimpleBasicAuth(u, p))
+	}
 	// myHandler requires HTTP Basic Auth to access
 	goji.Get("/*", http.FileServer(http.Dir("./docs")))
 	goji.Serve()
